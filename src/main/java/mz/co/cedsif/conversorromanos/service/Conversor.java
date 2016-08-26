fccd301a2d0372487a74a6729976b30d9655e0b7
Marcel Saraiva <mdfsaraiva@gmail.com>
2016-08-26 15:46:39 +0200

    Resolvendo cenario de teste de combinacoes invalidas

--- src/main/java/mz/co/cedsif/conversorromanos/service/Conversor.java
+++ src/main/java/mz/co/cedsif/conversorromanos/service/Conversor.java
@@ -3,34 +3,42 @@ package mz.co.cedsif.conversorromanos.service;
 import mz.co.cedsif.conversorromanos.model.NumeroRomano;
 
 public class Conversor {
-	
+
 	GestorNumero gestor;
-	
-	public Conversor()
-	{
+
+	public Conversor() {
 		gestor = new GestorNumero();
 	}
-	
-	public String converter(String numeroConvertido)
-	{
-		char[]numeros = numeroConvertido.toLowerCase().toCharArray();
-		int resultado=0;
-		NumeroRomano numeroAnterior = null;
+
+	public String converter(String numeroConvertido) {
+		char[] numeros = numeroConvertido.toLowerCase().toCharArray();
+		int resultado = 0;
+		NumeroRomano numeroUltimo = null;
+		NumeroRomano numeroPenultimo = null;
+		NumeroRomano numeroAntePenultimo = null;
 		NumeroRomano numeroActual = null;
-		for(int i=0;i<numeros.length;i++)
-		{
+		for (int i = 0; i < numeros.length; i++) {
 			numeroActual = gestor.devolveRomano(numeros[i]);
-			resultado+=numeroActual.getNumeroDecimal();
-			if(numeroAnterior!=null && numeroActual.getNumeroDecimal()>numeroAnterior.getNumeroDecimal())
-			{
-				if(numeroActual.getPosicao()-numeroAnterior.getPosicao()>2)
-				{
+			resultado += numeroActual.getNumeroDecimal();
+			if (numeroUltimo != null && numeroActual.getNumeroDecimal() > numeroUltimo.getNumeroDecimal()) {
+				if (numeroUltimo.getPosicao() % 2 == 1 || numeroActual.getPosicao() - numeroUltimo.getPosicao() > 2) {
 					throw new IllegalArgumentException();
 				}
-				resultado+=-2*numeroAnterior.getNumeroDecimal();
+				resultado += -2 * numeroUltimo.getNumeroDecimal();
 			}
-			numeroAnterior=numeroActual;
+			if (i >= 3) {
+				if (numeroActual.getNumeroRomano() == numeroUltimo.getNumeroRomano()
+						&& numeroUltimo.getNumeroRomano() == numeroPenultimo.getNumeroRomano()
+						&& numeroPenultimo.getNumeroRomano() == numeroAntePenultimo.getNumeroRomano()) {
+					throw new IllegalArgumentException();
+				}
+				numeroAntePenultimo = numeroPenultimo;
+			}
+			if (i >= 2) {
+				numeroPenultimo = numeroUltimo;
+			}
+			numeroUltimo = numeroActual;
 		}
-		return resultado+"";
+		return resultado + "";
 	}
 }
